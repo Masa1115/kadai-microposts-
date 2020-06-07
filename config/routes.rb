bd7804f706f4c77d40fdfafdcc59cf865dc95753
masaki.shiga <ma15sa99ma24@gmail.com>
2020-06-07 14:01:00 +0000

    microposts

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+
+  get 'microposts/create'
+  get 'microposts/destroy'
+  root to: 'toppages#index'
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get'signup', to: 'users#new'
+
+  resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
+
+end
